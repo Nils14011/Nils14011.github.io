59b2987f87c7ca928d28e7f6e8d334658a5c7f58
Nils14011 <Nils1401spam@gmail.com>
2025-05-24 21:21:48 +0200

    Add files via upload

--- Kitty Clicker/Testing.js
+++ Kitty Clicker/Testing.js
@@ -1,8 +1,10 @@
 let Counter = 0
-let firstCounter = 1
+let firstCounter = 0
 let secondCounter = 1
+let thirdCounter = 0
 let first = 10
 let second = 5
+let third = 250
 
 function myClicker() {
     Counter = Counter + secondCounter
@@ -13,12 +15,12 @@ function firstUpgrade() {
     if (Counter >= first) {
         firstCounter ++
         Counter = Counter - first
-        first = first * 2.2
+        first = first * 1.4
         document.getElementById("priceCatnip").innerHTML = Math.floor(first)
         setInterval (() => {
-            Counter ++
+            Counter = Counter + 1 * firstCounter
             document.getElementById("counter").innerHTML = Math.floor(Counter)
-        }, 1000 / firstCounter); 
+        }, 1000); 
     }
 }
 
@@ -29,5 +31,18 @@ function secondUpgrade () {
         second = second * 2.2
         document.getElementById("pricePetting").innerHTML = Math.floor(second)
         document.getElementById("counter").innerHTML = Math.floor(Counter)
+    }
+}
+
+function thirdUpgrade() {
+    if (Counter >= third) {
+        thirdCounter ++
+        Counter = Counter - third
+        third = third * 1.4
+        document.getElementById("priceWetfood").innerHTML = Math.floor(third)
+        setInterval (() => {
+            Counter = Counter + 5 * thirdCounter
+            document.getElementById("counter").innerHTML = Math.floor(Counter)
+        }, 1000); 
     }
 }
